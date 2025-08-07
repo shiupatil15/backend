ce00e923aa98902201b4b9d10e4e5b80a3722e8b
shiupatil15 <shivkanyayewale15@gmail.com>
2025-08-07 12:59:57 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,19 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  email: { type: String},
-  password: { type: String }
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
