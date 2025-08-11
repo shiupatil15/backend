54200685b75ca679ba9e82ddc87b2f7be293b0c4
shiupatil15 <shivkanyayewale15@gmail.com>
2025-08-11 12:24:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 5000;
 const path = require("path");
+const authRoutes = require("./routes/auth"); // path to auth.js
+app.use("/api", authRoutes);
 
 const cors = require("cors");
 app.use(cors());
